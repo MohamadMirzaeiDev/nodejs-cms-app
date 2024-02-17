0f7c160afdd79cf01005de2159e661571f75469c
MohamadMirzaeiDev <mohamadmirzaei.ir@gmail.com>
2024-02-18 00:37:43 +0330

    config swagger

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,21 @@
 import { NestFactory } from "@nestjs/core";
 import { AppModule } from "./app.module";
+import { DocumentBuilder, SwaggerModule } from "@nestjs/swagger";
 
 
 async function run(port:number){
     const app = await NestFactory.create(AppModule);
+    const config = new DocumentBuilder()
+        .setTitle('Node CMS App')
+        .setDescription('Powerful content managment system')
+        .setVersion('0.1.0')
+        .addTag('CMS')
+        .build()
+
+    const document = SwaggerModule.createDocument(app , config)
+
+    SwaggerModule.setup('api' , app , document);
+
     await app.listen(port)
 }
 
