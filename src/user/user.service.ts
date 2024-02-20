a7755782b3775223faadea1358746063b58a69d1
MohamadMirzaeiDev <mohamadmirzaei.ir@gmail.com>
2024-02-21 01:02:53 +0330

    change service

--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -1,22 +1,31 @@
 import { Injectable } from '@nestjs/common';
 import { CreateUserDto } from './dto/create-user.dto';
 import { UpdateUserDto } from './dto/update-user.dto';
+import { FindOptionsWhere, FindOptionsWhereProperty, Repository } from 'typeorm';
+import { User } from './entities/user.entity';
+import { InjectRepository } from '@nestjs/typeorm';
+
+type Where = FindOptionsWhere<User>
 
 @Injectable()
 export class UserService {
+  constructor(
+    @InjectRepository(User)
+    private readonly userRepo:Repository<User> , 
+  ){}
   create(createUserDto: CreateUserDto) {
     return 'This action adds a new user';
   }
 
-  findAll() {
-    return `This action returns all user`;
+  async findAll() :Promise<User[]>{
+    return this.userRepo.find({})
   }
 
-  findOne(id: number) {
-    return `This action returns a #${id} user`;
+  async findOne(where:Where) {
+    return await this.userRepo.findOne({where});
   }
 
-  update(id: number, updateUserDto: UpdateUserDto) {
+  async update(id: number, updateUserDto: UpdateUserDto) {
     return `This action updates a #${id} user`;
   }
 
