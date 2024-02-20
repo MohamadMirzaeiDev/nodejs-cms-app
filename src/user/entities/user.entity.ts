d825ca0d82655def1244d5987aef212229eae215
MohamadMirzaeiDev <mohamadmirzaei.ir@gmail.com>
2024-02-20 23:14:44 +0330

    create user entity

--- src/user/entities/user.entity.ts
+++ src/user/entities/user.entity.ts
@@ -1,1 +1,19 @@
-export class User {}
+import { Column, Entity, PrimaryGeneratedColumn } from "typeorm";
+
+@Entity({name : "User"})
+export class User {
+    @PrimaryGeneratedColumn('uuid')
+    id : string ;
+
+    @Column({type : 'varchar'})
+    first_name : string ; 
+
+    @Column({type : 'varchar'})
+    last_name : string ; 
+
+    @Column({type : 'varchar' , nullable : false})
+    email : string ;
+
+    @Column({type : 'varchar' , nullable : false})
+    password : string ; 
+}
