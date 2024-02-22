ab3a52091114cd8bada07099fa178157db66b265
MohamadMirzaeiDev <mohamadmirzaei.ir@gmail.com>
2024-02-22 22:19:53 +0330

    use validation pipe

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,12 @@
 import { NestFactory } from "@nestjs/core";
 import { AppModule } from "./app.module";
 import { DocumentBuilder, SwaggerModule } from "@nestjs/swagger";
+import { ValidationPipe } from "@nestjs/common";
 
 
 async function run(port:number){
     const app = await NestFactory.create(AppModule);
+    app.useGlobalPipes(new ValidationPipe())
     const config = new DocumentBuilder()
         .setTitle('Node CMS App')
         .setDescription('Powerful content managment system')
