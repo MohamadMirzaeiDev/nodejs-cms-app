09ee5db1ef5b37aa0ef8ee73ba920a83aa453f6e
MohamadMirzaeiDev <mohamadmirzaei.ir@gmail.com>
2024-02-22 22:20:18 +0330

    add property

--- src/user/dto/create-user.dto.ts
+++ src/user/dto/create-user.dto.ts
@@ -1,1 +1,17 @@
-export class CreateUserDto {}
+import { Role } from "../enum/role.enum";
+
+export class CreateUserDto {
+    id : string ;
+
+    first_name : string ; 
+
+    last_name : string ; 
+
+    email : string ;
+
+    username : string ; 
+
+    password : string ; 
+
+    role : Role ; 
+}
