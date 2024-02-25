19d93c92741bb18eab74a42dcfc809f5052a2bf5
MohamadMirzaeiDev <mohamadmirzaei.ir@gmail.com>
2024-02-25 23:16:37 +0330

    change logic

--- src/auth/decorator/role.decorator.ts
+++ src/auth/decorator/role.decorator.ts
@@ -1,27 +1,5 @@
-import { CanActivate, ExecutionContext, Injectable } from "@nestjs/common";
-import { Reflector } from "@nestjs/core";
-import { Observable } from "rxjs";
+import { SetMetadata } from "@nestjs/common";
 import { Role } from "src/user/enum/role.enum";
 
-@Injectable()
-export class HasRoles implements CanActivate {
-    constructor(
-        private readonly reflector:Reflector
-    ){}
-
-    canActivate(context: ExecutionContext): boolean | Promise<boolean> | Observable<boolean> {
-        const roles = this.reflector.getAllAndOverride<Role[]>(
-            'roles' , 
-            [
-                context.getHandler(),
-                context.getClass()
-        ])
-
-        if(!roles){
-            return false;
-        }
-
-        const {user} = context.switchToHttp().getRequest()
-        return roles.some((role)=>user.role?.include(role));
-    }
-}
+export const ROLES_KEY = 'role';
+export const HasRole = (...roles:Role[])=> SetMetadata(ROLES_KEY , roles)
