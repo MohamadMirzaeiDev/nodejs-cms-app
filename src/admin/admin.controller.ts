52e70c315cef32bda18849ca57c9eb081bef832f
MohamadMirzaeiDev <mohamadmirzaei.ir@gmail.com>
2024-02-25 12:00:19 +0330

    update document

--- src/admin/admin.controller.ts
+++ src/admin/admin.controller.ts
@@ -1,8 +1,10 @@
 import { Controller, Get, Req, UseGuards } from "@nestjs/common";
 import { AuthGuard } from "@nestjs/passport";
+import { ApiTags } from "@nestjs/swagger";
 import { Request } from 'express';
 import { JwtAuthGuard } from "src/auth/guards/jwt-auth.guard";
 
+@ApiTags('Admin')
 @Controller('admin')
 export class AdminController {
 
--- src/auth/dto/sign-in.dto.ts
+++ src/auth/dto/sign-in.dto.ts
@@ -1,9 +1,12 @@
+import { ApiProperty } from "@nestjs/swagger";
 import { IsNotEmpty, Length } from "class-validator";
 
 export class SignInDto {
+    @ApiProperty({type : 'string'})
     @IsNotEmpty()
     username : string ;
     
+    @ApiProperty()
     @IsNotEmpty()
     password : string ; 
 }
