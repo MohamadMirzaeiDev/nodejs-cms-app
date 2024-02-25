830262067d7c6aec92ef08869be326398b12ef27
MohamadMirzaeiDev <mohamadmirzaei.ir@gmail.com>
2024-02-25 23:17:03 +0330

    add api property (swagger)

--- src/user/dto/create-user.dto.ts
+++ src/user/dto/create-user.dto.ts
@@ -1,27 +1,37 @@
 import { IsNotEmpty, IsString } from "class-validator";
 import { Role } from "../enum/role.enum";
+import { ApiProperty } from "@nestjs/swagger";
 
 export class CreateUserDto {
+    @ApiProperty()
     @IsNotEmpty()
     @IsString()
     first_name : string ; 
     
+    @ApiProperty()
     @IsNotEmpty()
     @IsString()
     last_name : string ; 
 
+    @ApiProperty()
     @IsNotEmpty()
     @IsString()
     email : string ;
-    
+
+
+    @ApiProperty()
     @IsNotEmpty()
     @IsString()
     username : string ; 
     
+
+    @ApiProperty()
     @IsNotEmpty()
     @IsString()
     password : string ; 
 
+
+    @ApiProperty()
     @IsNotEmpty()
     roles : Role[] ; 
 }
