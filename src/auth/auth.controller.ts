46c85bfd51a03ed8437122b2f4f345eae17e75bd
MohamadMirzaeiDev <mohamadmirzaei.ir@gmail.com>
2024-02-25 23:16:06 +0330

    make api tag

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -2,7 +2,9 @@ import { Body, Controller, Get, Post } from '@nestjs/common';
 import { SignUpDto } from './dto/sign-up.dto';
 import { AuthService } from './auth.service';
 import { SignInDto } from './dto/sign-in.dto';
+import { ApiTags } from '@nestjs/swagger';
 
+@ApiTags('Auth')
 @Controller('auth')
 export class AuthController {
     constructor(
