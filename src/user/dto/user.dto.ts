c493ffea8ef3e5e91d7d8c3e29f85a15c56c9871
MohamadMirzaeiDev <mohamadmirzaei.ir@gmail.com>
2024-02-27 21:04:52 +0330

    update : add other fields

--- src/user/dto/user.dto.ts
+++ src/user/dto/user.dto.ts
@@ -8,6 +8,13 @@ export class UserDto {
     first_name : string ;
     last_name : string ;
     roles : Role[];
+    home_phone_number : string;
+    phone_number : string ;  
+    address : string ;
+    country : string ;    
+    city : string ; 
+    postal_code: string ;
+    note : string ; 
 
     constructor(user:any){
         this.id = user.id ;
@@ -16,5 +23,12 @@ export class UserDto {
         this.first_name = user.first_name ;
         this.last_name = user.last_name ;
         this.roles = user.roles ;
+        this.address = user.address ;
+        this.city = user.city ;
+        this.country = user.country ;
+        this.phone_number = user.phone_number ;
+        this.home_phone_number = user.home_phone_number ;
+        this.postal_code = user.postal_code ;
+        this.note = user.note ;
     }
 }
