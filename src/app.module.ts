0d440e6a76e8626ed2dc567b3ea5d477a9741fb4
MohamadMirzaeiDev <mohamadmirzaei.ir@gmail.com>
2024-03-23 11:44:54 +0330

    Add task module

--- src/app.module.ts
+++ src/app.module.ts
@@ -14,6 +14,7 @@ import { CategoryModule } from './category/category.module';
 import { OrderModule } from './order/order.module';
 import { CouponModule } from './coupon/coupon.module';
 import { ManagerModule } from "./manager/manager.module";
+import { TaskModule } from './task/task.module';
 
 
 @Module({
@@ -28,7 +29,7 @@ import { ManagerModule } from "./manager/manager.module";
         ProductModule,
         OrderModule,
         CouponModule , 
-        ManagerModule
+        ManagerModule, TaskModule
     ],
     controllers : [],
     exports : []
